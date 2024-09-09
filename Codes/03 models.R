bddb28e56cb40ccadbac32569fca50e5a7442383
Ismael Soto <isma-sa@hotmail.com>
2024-09-09 23:45:56 +0200

    Create 03 models.R

---
+++ Codes/03 models.R
@@ -0,0 +1,52 @@
+setwd("C:/Users/Propietario/Desktop/Pre_Post_invasion")
+data = read_xlsx("Effect_size_corrected.xlsx")
+data$G_Hedges <- as.numeric(data$G_Hedges)
+data$variance_G <- as.numeric(data$variance_G)
+
+data[data == 'Inf' | data == '-Inf'] <- NA
+
+data1<- data %>% filter(G_Hedges < 5)
+data1<- data1 %>% filter(G_Hedges > -5)
+
+
+meta_6 <- metafor::rma.mv(G_Hedges,  variance_G, 
+                          random = ~ 1 | Country/River/site_id,
+                          data = data1, method = "REML")
+
+summary(meta_6)
+
+
+  
+data1$Stage <- as.factor(data1$Stage)
+data1$Stage <- relevel(data1$Stage, ref = "early")
+meta_7 <- metafor::rma.mv(G_Hedges,  variance_G, 
+                          random = ~ 1 | Country/River/site_id,
+                          mods = ~ Stage - 1,
+                          data = data1, method = "REML")
+summary(meta_7)
+
+
+data11 <- data1 %>%
+  mutate(taxa = ifelse(taxa %in% c( "FEve_eco","FEve_bio", "FDis_eco","FDis_bio",
+                                   "Richness"), taxa, "Abundance"))
+
+meta_7.1 <- metafor::rma.mv(G_Hedges,  variance_G, 
+                          random = ~ 1 | Country/River/site_id,
+                          mods = ~ taxa+ Stage:taxa,
+                          data = data11, method = "REML")
+summary(meta_7.1)
+  
+
+meta_7.2 <- metafor::rma.mv(G_Hedges, variance_G, 
+                          random = ~ 1 | Country/River/site_id,
+                          mods = ~ sp_community + Stage:sp_community,
+                          data = data11, method = "REML")
+summary(meta_7.2)
+
+
+meta_7.3 <- metafor::rma.mv(G_Hedges, variance_G, 
+                            random = ~ 1 | Country/River/site_id,
+                            mods = ~   Taxo_Func + Stage:Taxo_Func,
+                            data = data11, method = "REML")
+summary(meta_7.3)
+
