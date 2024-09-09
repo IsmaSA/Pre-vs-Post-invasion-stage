5afa3a322636834021cab793736c200e4d63b42a
Ismael Soto <isma-sa@hotmail.com>
2024-09-09 23:50:01 +0200

    Update Publication bias.R

--- Codes/Publication bias.R
+++ Codes/Publication bias.R
@@ -6,3 +6,18 @@ for (i in 1:lenght(meta)){
   eggers <- regtest(x = resid$resid, sei =sqrt(data1$variance_G), model = "lm") 
   print(eggers)
 }
+
+
+
+# Funnel plot:
+biases <- list(meta_7, meta_7.1, meta_7.2, meta_7.3)
+par(mar=c(4, 4, 2, 2))
+
+p1<- funnel(meta6, xlab = "Effect Size", ylab = "Standard Error",back = "white")
+p2<- funnel(meta_7, xlab = "Effect Size", ylab = "Standard Error",back = "white")
+p3<- funnel(meta_7.1, xlab = "Effect Size", ylab = "Standard Error",back = "white")
+p4<- funnel(meta_7.2, xlab = "Effect Size", ylab = "Standard Error",back = "white")
+p5<- funnel(meta_7.3, xlab = "Effect Size", ylab = "Standard Error",back = "white")
+
+p2+ p3+
+  p4+p5
