e69966fe1e79bdf03327ef55f1e23ad153e9348d
Ismael Soto <isma-sa@hotmail.com>
2024-09-09 23:54:05 +0200

    Update Figure4.R

--- Codes/Figure4.R
+++ Codes/Figure4.R
@@ -1,5 +1,5 @@
 
-p1<- orchard_plot(object = meta_7.2, trunk.size = 12,
+p1<- orchard_plot(object = meta7.2, trunk.size = 10,
                   mod = "sp_community", 
                   group = "site_id", 
                   xlab = "Effect size (Hedges' G)") + 
@@ -15,26 +15,28 @@ p1<- orchard_plot(object = meta_7.2, trunk.size = 12,
 
 
 model_results <- data.frame(
-  term = c("Intercept", "Species", "Community:late", "Species:late"),
-  estimate = c(0.1119, -0.0460, 0.0211, -0.0498),
-  se = c(0.0236, 0.0139, 0.0184, 0.0138),
-  ci.lb = c(0.0657, -0.0732, -0.0150, -0.0769),
-  ci.ub = c(0.1582, -0.0188, 0.0572, -0.0227)
+  term = c("Community", "Species", "Community:late", "Species:late"),
+  estimate = c(0.1107, 0.0653, -0.1863, -0.1057),
+  se = c(0.0251, 0.0237, 0.0184, 0.0138),
+  zval = c(4.4189, 2.7567, -10.1122, -7.6582),
+  pval = c(0.00001, 0.0058, 0.00001, 0.00001),
+  ci.lb = c(0.0616, 0.0189, -0.2224, -0.1327),
+  ci.ub = c(0.1599, 0.1118, -0.1502, -0.0786)
 )
 model_results$term <- factor(model_results$term, levels = c(
-  "Community:late","Species:late","Species","Intercept"))
+  "Community:late","Community", "Species:late","Species"))
 
 p2<-ggplot(model_results, aes(x = estimate, y = term)) +
   geom_point(size=5) +
   geom_errorbarh(aes(xmin = ci.lb, xmax = ci.ub), height = 0.2, size=1.5) +
   xlab("Effect Size") +
   ylab("Model terms") + geom_vline(xintercept = 0, linetype="dashed", size=1.2)  + 
-  theme_bw()+ theme(axis.text.x = element_text(size = 12, color = "black"),
-                    axis.text.y = element_text(size = 12, vjust = 0.5, angle = 0, color = "black"),   
-                    axis.line.x = element_line(color = "black"),
-                    axis.line.y = element_line(color = "black"),
-                    axis.title = element_text(size = 14),
-                    panel.grid.major.y = element_blank(), 
-                    legend.position = "none")
+  theme_bw()+   theme(axis.text.x = element_text(size = 12, color = "black"),
+                      axis.text.y = element_text(size = 12, vjust = 0.5, angle = 0, color="black"),  
+                      axis.line.x = element_line(color = "black"),
+                      axis.line.y = element_line(color = "black"),
+                      axis.title = element_text(size = 14),
+                      panel.grid.major.y = element_blank(), 
+                      legend.position = "none")
 
 p1 + p2
